3a66eef257e0a66d38dc75b12cb812240290f6d8
xao1215 <allexxpez@gmail.com>
2022-10-14 00:17:29 +0200

    init

---
+++ src/main/java/com/project/cg_backend/CgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.cg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgBackendApplication.class, args);
+    }
+
+}
